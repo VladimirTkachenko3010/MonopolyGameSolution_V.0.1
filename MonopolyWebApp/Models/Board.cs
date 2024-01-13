91fc4f23f53c7f8fa2ddc997985acbcae81e8ba5
VladimirTkachenko3010 <v.s.tkachenko@student.khai.edu>
2024-01-13 19:02:24 +0200

    Added Properties on Board

--- MonopolyWebApp/Models/Board.cs
+++ MonopolyWebApp/Models/Board.cs
@@ -2,6 +2,8 @@
 {
     public class Board
     {
+        #region Свойства доски
+
         /// <summary>
         /// GameId for Board
         /// </summary>
@@ -16,5 +18,52 @@
         /// BranchId for Board
         /// </summary>
         public int BranchdID { get; set; }
+
+        #endregion
+
+        public Board() 
+        {
+            Property property1 = new Property("Олд Кент Роад", 60, 2, 10, 30, 90, 160, 250, 30, false, 50, "Brown", "OWNER");
+            //Общественная казна
+            Property property2 = new Property("Уайтшапель Роад",60 );
+            //Налог -200
+            Property property3 = new Property("Кинг Кросс Станция", 200 );
+            Property property4 = new Property("Анджел, Ислингтон", 100);
+            //Chance
+            Property property5 = new Property("Иустон Роад", 100);
+            Property property6 = new Property("Пентонвиль Роад",120 );
+            //Jail
+            Property property7 = new Property("Пэлл Мэлл", 140);
+            Property property8 = new Property("Электрическая компания", 150);
+            Property property9 = new Property("Уайтхолл", 140 );
+            Property property10 = new Property("Нортумберлэнд Авеню", 160);
+            Property property11 = new Property("Марилебоне Станция", 200);
+            Property property12 = new Property("Боу Стрит", 180 );
+            //Общественная казна
+            Property property13 = new Property("Малборо стрит", 180);
+            Property property14 = new Property("Вайн стрит", 200);
+            //Car
+            Property property15 = new Property("Стрэнд", 220);
+            //Chance
+            Property property16 = new Property("Флит Стрит", 220);
+            Property property17= new Property("Трафальгар Площадь",240 );
+            Property property18 = new Property("Фенчорч Станция", 200 );
+            Property property19 = new Property("Личестер Площадь", 260);
+            Property property20 = new Property("Ковентри Стрит",260 );
+            Property property21 = new Property("Водная Станция", 150);
+            Property property22 = new Property("Пикадилли", 280);
+            //Jail
+            Property property23 = new Property("Регент Стрит", 300);
+            Property property24 = new Property("Оксфорд Стрит", 300);
+            //Общественная казна
+            Property property25 = new Property("Бонд Стрит", 320);
+            Property property26 = new Property("Ливерпуль Станция", 200);
+            //Chance
+            Property property27 = new Property("Парк Лейн",350 );
+            //Налог -100
+            Property property28 = new Property("Мейфэир", 400);
+
+
+        }
     }
 }
--- MonopolyWebApp/Models/Property.cs
+++ MonopolyWebApp/Models/Property.cs
@@ -4,50 +4,52 @@ namespace MonopolyWebApp.Models
 {
     public class Property
     {
+        #region Свойства собственности
+
         /// <summary>
         /// Property name
         /// </summary>
         public string Name { get; set; } = string.Empty;
         /// <summary>
-        /// First buy price
+        /// Buy price
         /// </summary>
-        public int Price { get; set; }
+        public ushort Price { get; set; }
         /// <summary>
         /// First rent(without houses etc)
         /// </summary>
-        public int Rent { get; set; }
+        public ushort Rent { get; set; }
         /// <summary>
         /// Rent if 1 house on branch
         /// </summary>
-        public int Rent1House { get; set; }
+        public ushort Rent1House { get; set; }
         /// <summary>
         /// Rent if 2 house on branch
         /// </summary>
-        public int Rent2House { get; set; }
+        public ushort Rent2House { get; set; }
         /// <summary>
         /// Rent if 3 house on branch
         /// </summary>
-        public int Rent3House { get; set; }
+        public ushort Rent3House { get; set; }
         /// <summary>
         /// Rent if 4 house on branch
         /// </summary>
-        public int Rent4House { get; set; }
+        public ushort Rent4House { get; set; }
         /// <summary>
         /// Rent if Hotel on branch
         /// </summary>
-        public int RentHotel { get; set; }
+        public ushort RentHotel { get; set; }
         /// <summary>
         /// закладывание
         /// </summary>
-        public int MortageValue { get; set;}
+        public ushort MortageValue { get; set;}
         /// <summary>
         /// Не/Заложено
         /// </summary>
-        public bool IsMortage { get; set; } = false;
+        public bool IsMortagaged { get; set; } = false;
         /// <summary>
         /// Стоимость постройки
         /// </summary>
-        public int BuildingCost { get; set; }
+        public ushort BuildingCost { get; set; }
         /// <summary>
         /// Color of property
         /// </summary>
@@ -57,6 +59,41 @@ namespace MonopolyWebApp.Models
         /// </summary>
         public string Owner { get; set; }
 
+        #endregion
+
+        /// <summary>
+        /// Property ctor
+        /// </summary>
+        /// <param name="name">Property name</param>
+        /// <param name="price">Buy price</param>
+        /// <param name="rent">First rent(without houses etc)</param>
+        /// <param name="rent1House"> Rent if 1 house on branch</param>
+        /// <param name="rent2House"> Rent if 2 house on branch</param>
+        /// <param name="rent3House"> Rent if 3 house on branch</param>
+        /// <param name="rent4House"> Rent if 4 house on branch</param>
+        /// <param name="rentHotel"> Rent if Hotel on branch</param>
+        /// <param name="mortageValue">закладываемая стоимость</param>
+        /// <param name="isMortage">Не/Заложено</param>
+        /// <param name="buildingCost">Стоимость постройки</param>
+        /// <param name="color">Color of property</param>
+        /// <param name="owner">Owner of property</param>
+        public Property(string name, ushort price, ushort rent, ushort rent1House, ushort rent2House, ushort rent3House,
+            ushort rent4House, ushort rentHotel, ushort mortageValue, bool isMortagaged, ushort buildingCost, string color, string owner)
+        {
+            Name = name;
+            Price = price;
+            Rent = rent;
+            Rent1House = rent1House;
+            Rent2House = rent2House;
+            Rent3House = rent3House;
+            Rent4House = rent4House;
+            RentHotel = rentHotel;
+            MortageValue = mortageValue;
+            IsMortagaged = isMortagaged;
+            BuildingCost = buildingCost;
+            Color = color;
+            Owner = owner;
+        }
 
     }
 }
