2c6d9cf6e72c4a6f537d409abe706b59a312930c
VladimirTkachenko3010 <v.s.tkachenko@student.khai.edu>
2024-01-13 17:40:42 +0200

    Added Player properties + ctor

--- MonopolyWebApp/Models/Player.cs
+++ MonopolyWebApp/Models/Player.cs
@@ -1,26 +1,93 @@
-﻿namespace MonopolyWebApp.Models
+﻿using System.Collections;
+
+namespace MonopolyWebApp.Models
 {
     public class Player
     {
+        #region Свойства игрока
+       
         /// <summary>
         /// PlayerID of Player
         /// </summary>
         public int PlayerID { get; set; }
-
         /// <summary>
         /// UserID of Player
         /// </summary>
         public int UserID { get; set; }
-
         /// <summary>
         /// GameID of Player
         /// </summary>
         public int GameID { get; set; }
-
         /// <summary>
         /// PlayerMoney of Player
         /// </summary>
         public int PlayerMoney { get; set; }
+        /// <summary>
+        /// Карточки выхода из тюрьмы
+        /// </summary>
+        public ushort GetOutOfJailAmount { get; set; }
+        /// <summary>
+        /// Кол-во ходов в тюрьме
+        /// </summary>
+        public ushort JailTurnCount { get; set; }
+        /// <summary>
+        /// Count of doubles of dice
+        /// </summary>
+        public ushort DoublesCount{ get;set; }
+        /// <summary>
+        /// Position of Player on board
+        /// </summary>
+        public ushort BoardPos { get; set; }
+        /// <summary>
+        /// Not/InJail
+        /// </summary>
+        public bool InJail { get; set; } = false;
+        /// <summary>
+        /// Houses of player
+        /// </summary>
+        public ushort HouseAmount { get; set; }
+        /// <summary>
+        /// hotels of Player
+        /// </summary>
+        public ushort HotelAmount { get; set;}
+        /// <summary>
+        /// dice roll 2-12
+        /// </summary>
+        public ushort DiceRoll{ get; set;}
+        /// <summary>
+        /// The player's "position" in the player roster.
+        /// </summary>
+        public ushort RosterPosition { get; set;}
+        /// <summary>
+        /// Not/Passed go
+        /// </summary>
+        public bool PassedGo { get; set; } = false;
+        /// <summary>
+        /// Player prors
+        /// </summary>
+        ArrayList PlayerProperties = new ArrayList();
 
+        #endregion
+
+        //Default ctor
+        public Player()
+        {
+            PlayerID = 0;
+            UserID = 0;
+            GameID = 0;
+            //startMoney
+            PlayerMoney = 1500;
+            GetOutOfJailAmount = 0;
+            JailTurnCount = 0;
+            DoublesCount = 0;
+            BoardPos = 0;
+            InJail = false;
+            HouseAmount = 0;
+            HotelAmount = 0;
+            DiceRoll = 0;
+            RosterPosition = 0;
+            PassedGo = false;
+            PlayerProperties = new ArrayList();
+        }
     }
 }
---
+++ MonopolyWebApp/Models/Property.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MonopolyWebApp.Models
+{
+    public class Property
+    {
+        /// <summary>
+        /// Property name
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+        /// <summary>
+        /// First buy price
+        /// </summary>
+        public int Price { get; set; }
+        /// <summary>
+        /// First rent(without houses etc)
+        /// </summary>
+        public int Rent { get; set; }
+        /// <summary>
+        /// Rent if 1 house on branch
+        /// </summary>
+        public int Rent1House { get; set; }
+        /// <summary>
+        /// Rent if 2 house on branch
+        /// </summary>
+        public int Rent2House { get; set; }
+        /// <summary>
+        /// Rent if 3 house on branch
+        /// </summary>
+        public int Rent3House { get; set; }
+        /// <summary>
+        /// Rent if 4 house on branch
+        /// </summary>
+        public int Rent4House { get; set; }
+        /// <summary>
+        /// Rent if Hotel on branch
+        /// </summary>
+        public int RentHotel { get; set; }
+        /// <summary>
+        /// закладывание
+        /// </summary>
+        public int MortageValue { get; set;}
+        /// <summary>
+        /// Не/Заложено
+        /// </summary>
+        public bool IsMortage { get; set; } = false;
+        /// <summary>
+        /// Стоимость постройки
+        /// </summary>
+        public int BuildingCost { get; set; }
+        /// <summary>
+        /// Color of property
+        /// </summary>
+        public string Color { get; set; }
+        /// <summary>
+        /// Owner of property
+        /// </summary>
+        public string Owner { get; set; }
+
+
+    }
+}
