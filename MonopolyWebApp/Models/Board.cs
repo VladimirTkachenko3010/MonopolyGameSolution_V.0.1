d6fa756c09928c524afaa2bfd2e9449a4dc1151e
VladimirTkachenko3010 <v.s.tkachenko@student.khai.edu>
2024-01-14 22:20:36 +0200

    Added Properties on Board

--- MonopolyWebApp/Models/Board.cs
+++ MonopolyWebApp/Models/Board.cs
@@ -23,46 +23,50 @@
 
         public Board() 
         {
-            Property property1 = new Property("Олд Кент Роад", 60, 2, 10, 30, 90, 160, 250, 30, false, 50, "Brown", "OWNER");
-            //Общественная казна
-            Property property2 = new Property("Уайтшапель Роад",60 );
-            //Налог -200
-            Property property3 = new Property("Кинг Кросс Станция", 200 );
-            Property property4 = new Property("Анджел, Ислингтон", 100);
-            //Chance
-            Property property5 = new Property("Иустон Роад", 100);
-            Property property6 = new Property("Пентонвиль Роад",120 );
-            //Jail
-            Property property7 = new Property("Пэлл Мэлл", 140);
-            Property property8 = new Property("Электрическая компания", 150);
-            Property property9 = new Property("Уайтхолл", 140 );
-            Property property10 = new Property("Нортумберлэнд Авеню", 160);
-            Property property11 = new Property("Марилебоне Станция", 200);
-            Property property12 = new Property("Боу Стрит", 180 );
-            //Общественная казна
-            Property property13 = new Property("Малборо стрит", 180);
-            Property property14 = new Property("Вайн стрит", 200);
-            //Car
-            Property property15 = new Property("Стрэнд", 220);
-            //Chance
-            Property property16 = new Property("Флит Стрит", 220);
-            Property property17= new Property("Трафальгар Площадь",240 );
-            Property property18 = new Property("Фенчорч Станция", 200 );
-            Property property19 = new Property("Личестер Площадь", 260);
-            Property property20 = new Property("Ковентри Стрит",260 );
-            Property property21 = new Property("Водная Станция", 150);
-            Property property22 = new Property("Пикадилли", 280);
-            //Jail
-            Property property23 = new Property("Регент Стрит", 300);
-            Property property24 = new Property("Оксфорд Стрит", 300);
-            //Общественная казна
-            Property property25 = new Property("Бонд Стрит", 320);
-            Property property26 = new Property("Ливерпуль Станция", 200);
-            //Chance
-            Property property27 = new Property("Парк Лейн",350 );
-            //Налог -100
-            Property property28 = new Property("Мейфэир", 400);
+            #region Properties
 
+            //start
+            var property1 = new Property("Олд Кент Роад", 60, 2, 10, 30, 90, 160, 250, 30, false, 50, "Brown", "OWNER");
+            //Общественная казна
+            var property2 = new Property("Уайтшапель Роад", 60, 4, 20, 60, 180, 320, 450, 30, false, 50, "Brown", "OWNER" );
+            //Налог -200
+            var property3 = new Property("Кинг Кросс Станция", 200, 25, 25, 50, 100, 200, 0, 100, false, 1, "White", "OWNER"); //needs special logic
+            var property4 = new Property("Анджел, Ислингтон", 100, 6, 30, 90, 270, 400, 550, 50, false, 50, "Light Blue", "OWNER");
+            //Chance
+            var property5 = new Property("Иустон Роад", 100, 6, 30, 90, 270, 400, 550, 50, false, 50, "Light Blue", "OWNER");
+            var property6 = new Property("Пентонвиль Роад", 120,8, 40, 100, 300, 450, 600, 60, false, 50, "Light Blue", "OWNER");
+            //Jail
+            var property7 = new Property("Пэлл Мэлл", 140, 10, 50, 150, 450, 625, 750, 70, false, 100, "Pink", "OWNER");
+            var property8 = new Property("Электрическая компания", 150, 0, 0, 0, 0, 0, 0, 0, false, 0, "White2", "OWNER");  //needs sp logic
+            var property9 = new Property("Уайтхолл", 140, 10, 50, 150, 450, 625, 750, 70, false, 100, "Pink", "OWNER");
+            var property10 = new Property("Нортумберлэнд Авеню", 160, 12, 60, 180, 500, 700, 900, 80, false, 100, "Pink", "OWNER");
+            var property11 = new Property("Марилебоне Станция", 200,25, 25, 50, 100, 200, 0, 100, false, 0, "White", "OWNER"); //needs sp logic
+            var property12 = new Property("Боу Стрит", 180, 14, 70, 200, 550, 750, 950, 90, false, 100, "Orange", "OWNER");
+            //Общественная казна
+            var property13 = new Property("Малборо стрит", 180,14, 70, 200, 550, 750, 950, 90, false, 100, "Orange", "OWNER");
+            var property14 = new Property("Вайн стрит", 200,16, 80, 220, 600, 800, 1000, 100, false, 100, "Orange", "OWNER");
+            //Car
+            var property15 = new Property("Стрэнд", 220,18, 90, 250, 700, 875, 1050, 110, false, 150, "Red", "OWNER");
+            //Chance
+            var property16 = new Property("Флит Стрит", 220,18, 90, 250, 700, 875, 1050, 110, false, 150, "Red", "OWNER");
+            var property17 = new Property("Трафальгар Площадь", 240, 20, 100, 300, 750, 925, 1100, 120, false, 150, "Red", "OWNER");
+            var property18 = new Property("Фенчорч Станция", 200, 25, 25, 50, 100, 200, 0, 100, false, 0, "White", "OWNER");
+            var property19 = new Property("Личестер Площадь", 260, 22, 110, 330, 800, 975, 1150, 130, false, 150,  "Yellow", "OWNER");
+            var property20 = new Property("Ковентри Стрит", 260, 22, 110, 330, 800, 975, 1150, 130, false, 150, "Yellow", "OWNER");
+            var property21 = new Property("Водная Станция", 150, 0, 0, 0, 0, 0, 0, 75, false, 0, "White2", "OWNER");  //needs sp logic
+            var property22 = new Property("Пикадилли", 280, 24, 120, 360, 850, 1025, 1200, 140, false, 150, "Yellow", "OWNER");
+            //Jail
+            var property23 = new Property("Регент Стрит", 300, 26, 130, 390, 900, 1100, 1275, 150, false, 200, "Green", "OWNER");
+            var property24 = new Property("Оксфорд Стрит", 300, 26, 130, 390, 900, 1100, 1275, 150, false, 200, "Green", "OWNER");
+            //Общественная казна
+            var property25 = new Property("Бонд Стрит", 320,28, 150, 450, 1000, 1200, 1400, 160, false, 200, "Green", "OWNER");
+            var property26 = new Property("Ливерпуль Станция", 200,25, 25, 50, 100, 200, 0,100, false,0,"White", "OWNER");
+            //Chance
+            var property27 = new Property("Парк Лейн", 350,35, 175, 500, 1100, 1300, 1500, 175, false, 200, "Dark Blue", "OWNER");
+            //Налог -100
+            var property28 = new Property("Мейфэир", 400,50, 200, 600, 1400, 1700, 2000, 200, false, 200, "Dark Blue", "OWNER");
+
+            #endregion
 
         }
     }
