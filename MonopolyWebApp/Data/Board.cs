f415907e6b96ba301e28fa85ecbcdd6765cb4286
VladimirTkachenko3010 <v.s.tkachenko@student.khai.edu>
2024-01-26 18:43:53 +0200

    Added new spaceContainer and put all props in it

--- MonopolyWebApp/Data/Board.cs
+++ MonopolyWebApp/Data/Board.cs
@@ -1,4 +1,6 @@
-﻿namespace MonopolyWebApp.Models
+﻿using MonopolyWebApp.Data;
+
+namespace MonopolyWebApp.Models
 {
     public class Board
     {
@@ -19,6 +21,11 @@
         /// </summary>
         public int BranchdID { get; set; }
 
+        /// <summary>
+        /// An array of spaces that represent the game board.
+        /// </summary>
+        private Space[] spaceContainer;
+
         #endregion
 
         public Board() 
@@ -27,9 +34,9 @@
 
             //start
             var property1 = new Property("Олд Кент Роад", 60, 2, 10, 30, 90, 160, 250, 30, false, 50, "Brown", "OWNER");
-            //Общественная казна
+            //Общественная казна "Community Chest"
             var property2 = new Property("Уайтшапель Роад", 60, 4, 20, 60, 180, 320, 450, 30, false, 50, "Brown", "OWNER" );
-            //Налог -200
+            //Налог -200 "Income Tax"
             var property3 = new Property("Кинг Кросс Станция", 200, 25, 50, 100, 200, 0, 0, 100, false, 0, "Railroad", "OWNER"); 
             var property4 = new Property("Анджел, Ислингтон", 100, 6, 30, 90, 270, 400, 550, 50, false, 50, "Light Blue", "OWNER");
             //Chance
@@ -42,10 +49,10 @@
             var property10 = new Property("Нортумберлэнд Авеню", 160, 12, 60, 180, 500, 700, 900, 80, false, 100, "Pink", "OWNER");
             var property11 = new Property("Марилебоне Станция", 200, 25, 50, 100, 200, 0, 0, 100, false, 0, "Railroad", "OWNER");
             var property12 = new Property("Боу Стрит", 180, 14, 70, 200, 550, 750, 950, 90, false, 100, "Orange", "OWNER");
-            //Общественная казна
+            //Общественная казна "Community Chest"
             var property13 = new Property("Малборо стрит", 180,14, 70, 200, 550, 750, 950, 90, false, 100, "Orange", "OWNER");
             var property14 = new Property("Вайн стрит", 200,16, 80, 220, 600, 800, 1000, 100, false, 100, "Orange", "OWNER");
-            //Car
+            //"Free Parking"
             var property15 = new Property("Стрэнд", 220,18, 90, 250, 700, 875, 1050, 110, false, 150, "Red", "OWNER");
             //Chance
             var property16 = new Property("Флит Стрит", 220,18, 90, 250, 700, 875, 1050, 110, false, 150, "Red", "OWNER");
@@ -58,14 +65,24 @@
             //Jail
             var property23 = new Property("Регент Стрит", 300, 26, 130, 390, 900, 1100, 1275, 150, false, 200, "Green", "OWNER");
             var property24 = new Property("Оксфорд Стрит", 300, 26, 130, 390, 900, 1100, 1275, 150, false, 200, "Green", "OWNER");
-            //Общественная казна
+            //Общественная казна "Community Chest"
             var property25 = new Property("Бонд Стрит", 320,28, 150, 450, 1000, 1200, 1400, 160, false, 200, "Green", "OWNER");
             var property26 = new Property("Ливерпуль Станция", 200, 25, 50, 100, 200, 0, 0, 100, false, 0, "Railroad", "OWNER");
             //Chance
             var property27 = new Property("Парк Лейн", 350,35, 175, 500, 1100, 1300, 1500, 175, false, 200, "Dark Blue", "OWNER");
-            //Налог -100
+            //Налог -100 "Luxury Tax"
             var property28 = new Property("Мейфэир", 400,50, 200, 600, 1400, 1700, 2000, 200, false, 200, "Dark Blue", "OWNER");
 
+            // Put those properties into the spaceContainer, along with spaces that are not properties.
+            spaceContainer = new Space[40] { new Space("GO"), new Space(property1), new Space("Community Chest"),
+                new Space(property2), new Space("Income Tax"), new Space(property3), new Space(property4), new Space("Chance"),
+                new Space(property5), new Space(property6), new Space("Just Visiting"), new Space(property7), new Space(property8),
+                new Space(property9), new Space(property10), new Space(property11), new Space(property12), new Space("Community Chest"),
+                new Space(property13), new Space(property14), new Space("Free Parking"), new Space(property15), new Space("Chance"),
+                new Space(property16), new Space(property17), new Space(property18), new Space(property19), new Space(property20),
+                new Space(property21), new Space(property22), new Space("Go To Jail"), new Space(property23), new Space(property24),
+                new Space("Community Chest"), new Space(property25), new Space(property26), new Space("Chance"), new Space(property27),
+                new Space("Luxury Tax"), new Space(property28) };
             #endregion
 
         }
