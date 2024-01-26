790f8903b40d733d91c5ea670af64e11e65e97a5
VladimirTkachenko3010 <v.s.tkachenko@student.khai.edu>
2024-01-26 19:14:59 +0200

    Added new deck cs for chance and community cards

---
+++ MonopolyWebApp/Data/Deck.cs
@@ -0,0 +1,35 @@
+﻿using MonopolyWebApp.Models;
+using System.Collections.Generic;
+
+namespace MonopolyWebApp.Data
+{
+    public class Deck
+    {
+        /// <summary>
+        /// default ctor
+        /// </summary>
+        public Deck()
+        {
+            var deck = new List<Card>();
+        }
+
+        /// <summary>
+        /// ctor that initializes a deck of cards specified by the string passed in.
+        /// </summary>
+        /// <param name="deckName"></param>
+        public Deck(string deckName)
+        {
+            // Only initialize the deck if the name is either "Chance" or "Community."
+            if (deckName == "Chance")
+            {
+                // Add the Chance cards to the deck.
+                var deck = new List<Card>();
+                deck.Add(new Card("desc", "action"));
+            }
+            else if (deckName == "Community")
+            {
+                //
+            }
+        }
+    }
+}
